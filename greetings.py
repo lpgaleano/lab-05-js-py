19e9ffac595721349574d020c4828325f3d1bcb6
Lorena Galeano <lorenagaleano@Lorenas-MBP-2.home>
2021-10-04 16:43:24 -0400

    commiting a few files

---
+++ greetings.py
@@ -0,0 +1,7 @@
+ username = input("Hey what's your name? ")
+    # username is a symbol representing a placeholder in memoiry
+    #input()is a function that collects user input
+    #the output of the input() funcation is assigned to the variable username
+print("Hi " + str(username) + "! ")
+    # console.log() is a print function
+    # str() converts its input a strings 
